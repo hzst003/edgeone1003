1b7030f206a65c41a9324374c2eb8a8dc6c4ee2a
xu <you@example.com>
2025-10-15 20:10:59 -0500

    test10

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
 reactStrictMode: true,
   images: {
     unoptimized: true, // 防止 next/image 优化出错
-    domains: ["edgeone.hzst.online"], // 如果引用外部图像
+    domains: ["edgeone.hzst.online", "https://net-cloud.xhey.top"], // 如果引用外部图像
   },
 };
 
