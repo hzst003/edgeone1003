932131d0aa1ffe84fe3b7fad609fcb79befddbe6
xu <you@example.com>
2025-10-15 20:28:35 -0500

    test11

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,17 @@ const nextConfig: NextConfig = {
 reactStrictMode: true,
   images: {
     unoptimized: true, // 防止 next/image 优化出错
-    domains: ["edgeone.hzst.online", "https://net-cloud.xhey.top"], // 如果引用外部图像
+    domains: ["edgeone.hzst.online"], // 如果引用外部图像
+     remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'net-cloud.xhey.top',
+        pathname: '/group/photo/**', // 匹配该路径下所有图片
+      },
+    ],
   },
 };
+
+
 
 export default nextConfig;
